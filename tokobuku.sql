a3e7f713e8afd21177d90d5a9e02b14c3c9d21b1
putryagustin <94770364+putryagustin@users.noreply.github.com>
2023-01-15 19:08:17 +0700

    Add files via upload

---
+++ tokobuku.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Jan 2023 pada 12.57
+-- Versi server: 10.4.17-MariaDB
+-- Versi PHP: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tokobuku`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `dbbuku`
+--
+
+CREATE TABLE `dbbuku` (
+  `kode_buku` varchar(10) NOT NULL,
+  `jenis_buku` varchar(40) NOT NULL,
+  `penerbit` varchar(40) NOT NULL,
+  `harga_buku` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `dbbuku`
+--
+ALTER TABLE `dbbuku`
+  ADD PRIMARY KEY (`kode_buku`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
